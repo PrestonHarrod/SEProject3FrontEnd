85c8df79d222303183e1c454a2aec5f797f40819
PrestonHarrod <prestonharrod@gmail.com>
2021-10-13 10:02:49 -0500

    adding login page

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+//import store from './store/store'
+
+import GoogleAuth from '@/config/google_oAuth.js'
+
+const gauthOption = {
+  clientId: 'xxxxxxxxxxx.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+Vue.use(GoogleAuth, gauthOption)
+Vue.config.productionTip = false
+
 Vue.config.productionTip = false
 new Vue({
   router,
