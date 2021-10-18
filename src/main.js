0789d5db98b84a2bc3a89416ff23da5f379b1060
PrestonHarrod <prestonharrod@gmail.com>
2021-10-18 11:37:44 -0500

    add login page files from tutorial

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
+import GoogleAuth from '@/config/google_oAuth.js'
+const gauthOption = {
+  clientId: 'xxxxxxxxxxx.apps.googleusercontent.com',
+  scope: 'profile email',
+  prompt: 'select_account'
+}
+
 //Vue.config.productionTip = false
 new Vue({
   router,
   vuetify,
+  gauthOption,
+  GoogleAuth,
   render: h => h(App)
 }).$mount('#app')
 
